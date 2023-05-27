5a28fac173dfa77a7d890538ff29a0f4168bd525
eggfry313 <chanjung4505@gmail.com>
2023-05-27 23:47:32 +0900

    learned javascript template literal

--- syntax/template.js
+++ syntax/template.js
@@ -1,4 +1,9 @@
 var name = 'javascript';
 var Sentences = 'Practice ' + name + ' Sentences Using Variables';
 console.log(Sentences);
-// 코드의 가독성 증가, 코드내의 중복 제거
+
+var name = 'javascript';
+var Sentences = `Practice ${name}
+
+Sentences Using Variables`;
+console.log(Sentences);
