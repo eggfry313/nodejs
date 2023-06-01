5273974a8cfc276c49b9306a18beea5fc968f1be
eggfry313 <chanjung4505@gmail.com>
2023-06-01 16:10:58 +0900

    JavaScript Conditional statement

--- syntax/template.js
+++ syntax/template.js
@@ -1,9 +1,8 @@
 var name = 'javascript';
-var Sentences = 'Practice ' + name + ' Sentences Using Variables';
-console.log(Sentences);
-
-var name = 'javascript';
 var Sentences = `Practice ${name}
-
 Sentences Using Variables`;
 console.log(Sentences);
+
+// template literal(템플릿 리터럴) javascript에서의 문자 표현방법
+// 표현법 `String` 
+// template literal에서의 변수 ${변수명}
