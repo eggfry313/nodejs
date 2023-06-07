d852ca5a44c9945759c88366b0ad0ad936af7da7
eggfry313 <106800191+eggfry313@users.noreply.github.com>
2023-06-07 20:05:37 +0900

    file이 추가됨에 따라 글 목록에도 자동으로 파일을 생성하게끔 만듬

--- main.js
+++ main.js
@@ -2,16 +2,55 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (err, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = '<ul>';
+
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}<a></li>`
+          i = i + 1;
+        }
+        list = list + '</ul>';
+
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir('./data', function (err, filelist) {
+        var title = queryData.id;
+        var list = '<ul>';
+
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}<a></li>`
+          i = i + 1;
+        }
+        list = list + '</ul>';
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
           var template = `
           <!doctype html>
           <html>
@@ -21,11 +60,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -34,37 +69,13 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(template);
         });
-      }else{
-        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-          var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
-      }
-    }else{
-      response.writeHead(404);
-      response.end('Not found');
+      });
     }
-      
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
 
 });
 app.listen(3000);
