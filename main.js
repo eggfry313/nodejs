1e0ce2ef5f7fa8d513f19719bd88c0d2239340c5
eggfry313 <106800191+eggfry313@users.noreply.github.com>
2023-06-15 17:25:22 +0900

    main.js can save user's files

--- main.js
+++ main.js
@@ -81,10 +81,11 @@ var app = http.createServer(function (request, response) {
       var post = qs.parse(body);
       var title = post.title;
       var description = post.description;
-      console.log(post);
+      fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+      });
     });
-    response.writeHead(200);
-    response.end('success');
   } else {
     response.writeHead(404);
     response.end('Not found');
