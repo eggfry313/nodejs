3c3ed2efbf8a788e75d6dade3dc036221e434fc1
eggfry313 <106800191+eggfry313@users.noreply.github.com>
2023-06-22 17:19:58 +0900

    make delete function in main.js

--- main.js
+++ main.js
@@ -53,8 +53,15 @@ var app = http.createServer(function (request, response) {
         fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          var template = templateHTML(title, list, 
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>
+             <a href="/update?id=${title}">update</a>
+             <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">  
+             </form>
+            `);
           response.writeHead(200);
           response.end(template);
         });
@@ -123,6 +130,19 @@ var app = http.createServer(function (request, response) {
           response.end();
         });
       });
+    });
+  } else if(pathname === '/delete_process'){
+    var body = '';
+    request.on('data', function (data) {
+      body += data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(err){
+        response.writeHead(302, { Location: `/` });
+          response.end();
+      })
     });
   } else {
     response.writeHead(404);
