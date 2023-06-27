ecf4d61f04d4cabe40df743b4f6194763a8be0b0
eggfry313 <106800191+eggfry313@users.noreply.github.com>
2023-06-27 20:18:17 +0900

    sanitize input value in main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js')
+var sanitizeHtml = require('sanitize-html')
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -24,13 +25,15 @@ var app = http.createServer(function (request, response) {
       fs.readdir('./data', function (err, filelist) {
         fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
           var title = queryData.id;
+          var sanitizeTitle = sanitizeHtml(title);
+          var sanitizeDescription = sanitizeHtml(description);
           var list = template.list(filelist);
           var html = template.HTML(title, list, 
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizeTitle}</h2>${sanitizeDescription}`,
             `<a href="/create">create</a>
-             <a href="/update?id=${title}">update</a>
+             <a href="/update?id=${sanitizeTitle}">update</a>
              <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${sanitizeTitle}">
               <input type="submit" value="delete">  
              </form>
             `);
